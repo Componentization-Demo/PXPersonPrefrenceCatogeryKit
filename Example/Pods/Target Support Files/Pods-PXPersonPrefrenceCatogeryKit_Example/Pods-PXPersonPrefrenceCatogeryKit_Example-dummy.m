16ad1abf7411605382a81bff1fe5482f69030d29
os <panxiangxing@example.com>
2019-08-22 11:25:33 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-PXPersonPrefrenceCatogeryKit_Example/Pods-PXPersonPrefrenceCatogeryKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PXPersonPrefrenceCatogeryKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PXPersonPrefrenceCatogeryKit_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-PXPersonPrefrenceCatogeryKit_Tests/Pods-PXPersonPrefrenceCatogeryKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PXPersonPrefrenceCatogeryKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PXPersonPrefrenceCatogeryKit_Tests
+@end
